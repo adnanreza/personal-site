fb0c6bdd9cae6021edcc6a429faedfcaae0647b0
Adnan Reza <adnan.reza@outlook.com>
2019-09-07 09:15:34 -0700

    Cretated footer component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   });
+
   return (
     <Router>
       <div className='App'>
         <Navbar></Navbar>
         Adnan's website
+        <Footer></Footer>
       </div>
     </Router>
   );
