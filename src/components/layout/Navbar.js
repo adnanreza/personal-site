60f67bb8157bc41749b6963ac45a12c30170fd23
Adnan Reza <adnan.reza@outlook.com>
2019-09-14 23:40:38 -0700

    Make navbar responsive

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,27 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='black' style={{ padding: '0 2rem' }}>
+    <nav>
       <div className='nav-wrapper'>
         <Link to='/' className='brand-logo'>
           Adnan Reza
         </Link>
-        <ul id='nav-mobile' className='right hide-on-med-and-down'>
-          <li>
-            <Link to='/Teaching'>Teaching</Link>
-          </li>
+        <Link to='#' data-target='mobile-demo' className='sidenav-trigger'>
+          <i className='material-icons'>menu</i>
+        </Link>
+        <ul className='right hide-on-med-and-down'>
           <li>
             <Link to='/Experience'>Experience</Link>
+          </li>
+          <li>
+            <Link to='/Teaching'>Teaching</Link>
           </li>
           <li>
             <Link to='/Projects'>Projects</Link>
           </li>
           <li>
-            <Link className='grey-text text-lighten-3' to='/Resume'>
-              Resume
-            </Link>
+            <Link to='/Resume'>Resume</Link>
+          </li>
+          <li>
+            <Link to='/Contact'>Contact</Link>
+          </li>
+        </ul>
+        <ul className='sidenav' id='mobile-demo'>
+          <li>
+            <Link to='/Experience'>Experience</Link>
+          </li>
+          <li>
+            <Link to='/Teaching'>Teaching</Link>
+          </li>
+          <li>
+            <Link to='/Projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/Resume'>Resume</Link>
           </li>
           <li>
             <Link to='/Contact'>Contact</Link>
