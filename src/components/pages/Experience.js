3c94c15ec1b2b84faf506327f3d3a29b6a037129
Adnan Reza <adnan.reza@outlook.com>
2019-09-16 17:56:22 -0700

    Updated Experience and Teaching Pages

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import ResumeBlock from '../layout/ResumeBlock';
 
 const Experience = () => {
-  return <div>Experience</div>;
+  const accomplishmentsPA = [
+    'Implemented web framework for UBC’s new common look and feel using Oracle PL/SQL, HTML5, CSS3, Bootstrap and JavaScript.',
+    'Developed SQL queries for reporting sales data; Performed ad hoc reporting using Oracle SQL.',
+    'Extracted, transformed and loaded data into Oracle database using Excel and SQL.',
+    'Developed and maintained web pages using PHP, HTML5/CSS3, JavaScript (vanilla and jQuery).',
+    'Documented database schemas and business logic using ER diagrams and flowcharts.'
+  ];
+  const accomplishmentsWDUBC = [
+    'Designed the Patients in Education (PIE) Website',
+    'Conducted needs assessment to identify features members want to see in the website',
+    'Created several design mock-ups, logos to get feedback from members.',
+    'Implemented Google Analytics to track website statistics and audience behavior.',
+    'Created documentation for easy website maintenance and updates in the future.'
+  ];
+  const accomplishmentsWDUBC2 = [
+    'Performed comprehensive internal assessment of existing CALP websites',
+    'Recommended ways to reduce hosting costs by streamlining the number of hosting providers based on the bandwidth and technology stack needs of each project.',
+    'Developed online case studies based on designs provided by CALP researchers.',
+    'Maintained existing CALP websites'
+  ];
+  const accomplishmentsMT = [
+    'Full Stack Web Development using HTML5/CSS3, JavaScript, PHP and MySQL.',
+    'Communicated with clients, translated user requirements into working applications and ensured on-time delivery.',
+    'Improved and modernized older websites.',
+    'Recommended ways to simplify the process of maintaining their websites via on-site training.'
+  ];
+
+  return (
+    <Fragment>
+      <div className='row'>
+        <div className='col s12'>
+          <h4>
+            <i className='fa fa-briefcase'></i>
+            {` `}
+            Software Development Experience
+          </h4>
+          <ResumeBlock
+            heading='Programmer Analyst - University of British Columbia IT'
+            subheading='June 2018 - July 2019'
+            body={accomplishmentsPA}
+            list={true}
+          ></ResumeBlock>
+          <ResumeBlock
+            heading='Web Developer - UBC Health'
+            subheading='January 2017 - April 2017'
+            body={accomplishmentsWDUBC}
+            list={true}
+          ></ResumeBlock>
+          <ResumeBlock
+            heading='Web Developer - UBC Forestry'
+            subheading='April 2017 - May 2017'
+            body={accomplishmentsWDUBC2}
+            list={true}
+          ></ResumeBlock>
+          <ResumeBlock
+            heading='Full Stack Web Developer - Matroid Technologies'
+            subheading='January 2014 - August 2015'
+            body={accomplishmentsMT}
+            list={true}
+          ></ResumeBlock>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Experience;
--- src/components/pages/Teaching.js
+++ src/components/pages/Teaching.js
@@ -3,73 +3,59 @@ import { Link } from 'react-router-dom';
 import CC from '../layout/columbia-college.png';
 import BU from '../layout/bracu.png';
 import UBCCS from '../layout/ubccs.png';
+import ResumeBlock from '../layout/ResumeBlock';
 
 const Teaching = () => {
+  const accomplishmentsGTA = [
+    'Designed, ran and managed laboratory and tutorial sessions',
+    'Designed, reviewed and graded assignments, projects and exams',
+    'Received a Graduate Teaching Assistant Award from the CS Department at UBC'
+  ];
+  const accomplishmentsAG = [
+    'Mentored potential CS graduate students',
+    'Held weekly one-on-one sessions focusing on the application process to CS graduate schools',
+    'Conducted interview practice sessions and helped students improve their communication skills',
+    'Advised on CS graduate school course selection',
+    'Guided students in post-degree career options'
+  ];
+  const accomplishmentsTABRAC = [
+    'Graded programs, quizzes and assignments.',
+    'Conducted tutorial sessions on an individual and group basis',
+    'Helped students with course material during office hours.',
+    'Identified weak students and held extra sessions to improve their performance',
+    'Discussed advanced topics not covered in lectures'
+  ];
+
   return (
     <Fragment>
-      <div className='container'>
-        <div class='section'>
-          <h5>Computer Science Instructor</h5>
-          <h7>
-            Department of Computer Science and Mathematics
-            <br />
-            Columbia College
-            <br />
-            Vancouver, BC
-          </h7>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-            nesciunt hic deserunt eum eveniet aspernatur numquam beatae fuga aut
-            nam?
-          </p>
-        </div>
-        <div class='divider'></div>
-        <div class='section'>
-          <h5>Graduate Teaching Assistant</h5>
-          <h7>
-            Department of Computer Science
-            <br />
-            University of British Columbia
-            <br />
-            Vancouver, BC
-          </h7>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-            nesciunt hic deserunt eum eveniet aspernatur numquam beatae fuga aut
-            nam?
-          </p>
-        </div>
-        <div class='divider'></div>
-        <div class='section'>
-          <h5>Teaching Assistant</h5>
-          <h7>
-            Department of Computer Science and Engineering
-            <br />
-            BRAC University
-            <br />
-            Dhaka, Bangladesh
-          </h7>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-            nesciunt hic deserunt eum eveniet aspernatur numquam beatae fuga aut
-            nam?
-          </p>
-        </div>
-        <div class='divider'></div>
-        <div class='section'>
-          <h5>Teaching Assistant</h5>
-          <h7>
-            Department of Economics
-            <br />
-            BRAC University
-            <br />
-            Dhaka, Bangladesh
-          </h7>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-            nesciunt hic deserunt eum eveniet aspernatur numquam beatae fuga aut
-            nam?
-          </p>
+      <div className='row'>
+        <div className='col s12'>
+          <h4>
+            <i class='fas fa-chalkboard-teacher'></i>
+            {` `}
+            Teaching Experience
+          </h4>
+
+          <ResumeBlock
+            heading='Graduate Teaching Assistant - UBC Computer Science Department'
+            subheading='September 2015 - April 2018'
+            body={accomplishmentsGTA}
+            list={true}
+          ></ResumeBlock>
+
+          <ResumeBlock
+            heading='Academic Guide - UBC Extended Learning'
+            subheading='January 2017 - December 2017'
+            body={accomplishmentsAG}
+            list={true}
+          ></ResumeBlock>
+
+          <ResumeBlock
+            heading='Teaching Assistant - BRAC University'
+            subheading='January 2013 - July 2015'
+            body={accomplishmentsTABRAC}
+            list={true}
+          ></ResumeBlock>
         </div>
       </div>
     </Fragment>
