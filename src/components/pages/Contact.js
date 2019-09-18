db6e719fedc776ea6b5ca06895f196389083cdc5
Adnan Reza <adnan.reza@outlook.com>
2019-09-17 22:08:54 -0700

    Started contact component

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Contact = props => {
-  return <div>Contact page</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const { name, email, message } = formData;
+
+  return (
+    <Fragment>
+      <div className='row'>
+        <form className='col s12'>
+          <h3>Leave a message</h3>
+          <div class='input-field col s6'>
+            <input
+              placeholder='Placeholder'
+              id='first_name'
+              type='text'
+              class='validate'
+            />
+            <label for='first_name'>First Name</label>
+          </div>
+          <div class='input-field col s6'>
+            <input id='last_name' type='text' class='validate' />
+            <label for='last_name'>Last Name</label>
+          </div>
+        </form>
+      </div>
+    </Fragment>
+  );
 };
 
 Contact.propTypes = {};
