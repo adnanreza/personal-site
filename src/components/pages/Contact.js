2bdbaa0e30dc67af67e0da85a62ce45ceef717f2
Adnan Reza <adnan.reza@outlook.com>
2019-09-20 19:19:56 -0700

    Refactor contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,104 +1,101 @@
-import React, { Fragment, useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import ContactContext from '../../context/contact/contactContext';
-import PropTypes from 'prop-types';
+import React, { Component, Fragment } from 'react';
 
-const Contact = props => {
-  const contactContext = useContext(ContactContext);
-  const [formData, setFormData] = useState({
-    fname: '',
-    lname: '',
-    email: '',
-    message: ''
-  });
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { fname: '', lname: '', email: '', message: '' };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-  const { fname, lname, email, message } = formData;
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-  const onChange = e =>
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
+  onSubmit(event) {
+    alert(
+      'Hi' +
+        ' ' +
+        this.state.fname +
+        ' ' +
+        this.state.lname +
+        '. Thank you for you message. Your email is ' +
+        this.state.email +
+        ' and your message is ' +
+        this.state.message
+    );
+    event.preventDefault();
+  }
 
-  const onSubmit = e => {
-    e.preventDefault();
-    console.log('Form Submitted');
-  };
+  render() {
+    return (
+      <Fragment>
+        <div className='container'>
+          <div className='row'>
+            <form className='col s12' onSubmit={this.onSubmit}>
+              <h3>Leave a message.</h3>
+              <br />
+              <br />
+              <div className='input-field col s6'>
+                <input
+                  placeholder='First Name'
+                  name='fname'
+                  type='text'
+                  className='validate'
+                  value={this.fname}
+                  onChange={this.onChange}
+                  required
+                />
+              </div>
+              <div className='input-field col s6'>
+                <input
+                  placeholder='Last Name'
+                  name='lname'
+                  type='text'
+                  className='validate'
+                  value={this.lname}
+                  onChange={this.onChange}
+                  required
+                />
+              </div>
 
-  return (
-    <Fragment>
-      <div className='container'>
-        <div className='row'>
-          <form className='col s12' onSubmit={e => onSubmit(e)}>
-            <h3>Leave a message.</h3>
-            <br />
-            <br />
-            <div className='input-field col s6'>
-              <input
-                placeholder='First Name'
-                name='first_name'
-                type='text'
-                className='validate'
-                value={fname}
-                onChange={e => onChange(e)}
-                required
-              />
-              <label htmlFor='first_name'>First Name</label>
-            </div>
-            <div className='input-field col s6'>
-              <input
-                placeholder='Last Name'
-                name='last_name'
-                type='text'
-                className='validate'
-                value={lname}
-                onChange={e => onChange(e)}
-                required
-              />
-              <label htmlFor='last_name'>Last Name</label>
-            </div>
+              <div className='input-field col s8'>
+                <input
+                  placeholder='Email'
+                  name='email'
+                  type='email'
+                  className='validate'
+                  value={this.email}
+                  onChange={this.onChange}
+                  required
+                />
+              </div>
 
-            <div className='input-field col s8'>
-              <input
-                placeholder='Email'
-                name='email'
-                type='email'
-                className='validate'
-                value={email}
-                onChange={e => onChange(e)}
-                required
-              />
-              <label htmlFor='email'>Email</label>
-            </div>
+              <div className='input-field col s12'>
+                <textarea
+                  name='message'
+                  maxLength='250'
+                  className='materialize-textarea'
+                  value={this.message}
+                  placeholder='Message'
+                  onChange={e => this.onChange(e)}
+                ></textarea>
+              </div>
 
-            <div className='input-field col s12'>
-              <textarea
-                name='textarea1'
-                maxLength='250'
-                className='materialize-textarea'
-                value={message}
-                onChange={e => onChange(e)}
-              ></textarea>
-              <label htmlFor='textarea1'>Message</label>
-            </div>
-
-            <div className='input-field col s6'>
-              <button
-                className='btn waves-effect waves-light'
-                type='submit'
-                name='action'
-              >
-                Submit
-                <i className='material-icons right'>send</i>
-              </button>
-            </div>
-          </form>
+              <div className='input-field col s6'>
+                <button
+                  className='btn waves-effect waves-light'
+                  type='submit'
+                  value='Submit'
+                >
+                  Submit
+                  <i className='material-icons right'>send</i>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-    </Fragment>
-  );
-};
-
-Contact.propTypes = {};
-
-export default Contact;
+      </Fragment>
+    );
+  }
+}
