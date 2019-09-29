011c459a7a8a6acc837bf82b65209a14ddda1d22
Adnan Reza <adnan.reza@outlook.com>
2019-09-29 09:42:49 -0700

    Made desktop nav fixed and fixed mobile nav

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,50 +3,56 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav style={style}>
-      <div className='nav-wrapper'>
-        <Link to='/' className='brand-logo' style={{ padding: '0 20px' }}>
-          Adnan Reza
-        </Link>
-        <Link to='#' data-target='mobile-demo' className='sidenav-trigger'>
-          <i className='material-icons'>menu</i>
-        </Link>
-        <ul className='right hide-on-med-and-down'>
-          <li>
-            <Link to='/Experience'>Experience</Link>
-          </li>
-          <li>
-            <Link to='/Teaching'>Teaching</Link>
-          </li>
-          <li>
-            <Link to='/Projects'>Projects</Link>
-          </li>
-          <li>
-            <Link to='/Resume'>Résumé</Link>
-          </li>
-          <li>
-            <Link to='/Contact'>Contact</Link>
-          </li>
-        </ul>
-        <ul className='sidenav' id='mobile-demo'>
-          <li>
-            <Link to='/Experience'>Experience</Link>
-          </li>
-          <li>
-            <Link to='/Teaching'>Teaching</Link>
-          </li>
-          <li>
-            <Link to='/Projects'>Projects</Link>
-          </li>
-          <li>
-            <Link to='/Resume'>Résumé</Link>
-          </li>
-          <li>
-            <Link to='/Contact'>Contact</Link>
-          </li>
-        </ul>
+    <Fragment>
+      <div className='navbar-fixed'>
+        <nav style={style}>
+          <div className='nav-wrapper'>
+            <Link to='/' className='brand-logo' style={{ padding: '0 20px' }}>
+              Adnan Reza
+            </Link>
+            <Link to='#' data-target='mobile-demo' className='sidenav-trigger'>
+              <i className='material-icons'>menu</i>
+            </Link>
+            <ul className='right hide-on-med-and-down'>
+              <li>
+                <Link to='/Experience'>Experience</Link>
+              </li>
+              <li>
+                <Link to='/Teaching'>Teaching</Link>
+              </li>
+              <li>
+                <Link to='/Projects'>Projects</Link>
+              </li>
+              <li>
+                <Link to='/Resume'>Résumé</Link>
+              </li>
+              <li>
+                <Link to='/Contact'>Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-    </nav>
+      {/**Mobile Navigation*/}
+      <ul className='sidenav' id='mobile-demo'>
+        <li>
+          <Link to='/Experience'>Experience</Link>
+        </li>
+        <li>
+          <Link to='/Teaching'>Teaching</Link>
+        </li>
+        <li>
+          <Link to='/Projects'>Projects</Link>
+        </li>
+        <li>
+          <Link to='/Resume'>Résumé</Link>
+        </li>
+        <li>
+          <Link to='/Contact'>Contact</Link>
+        </li>
+      </ul>
+      {/**Mobile Navigation*/}
+    </Fragment>
   );
 };
 
