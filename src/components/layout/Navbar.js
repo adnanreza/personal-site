156dbb3d498a4f1df184af2658e57c52077dbe9f
Adnan Reza <adnan.reza@outlook.com>
2019-09-28 23:55:42 -0700

    Removed navbar fixed class due to bug

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,52 +3,50 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='navbar-fixed'>
-      <nav style={style}>
-        <div className='nav-wrapper'>
-          <Link to='/' className='brand-logo' style={{ padding: '0 20px' }}>
-            Adnan Reza
-          </Link>
-          <Link to='#' data-target='mobile-demo' className='sidenav-trigger'>
-            <i className='material-icons'>menu</i>
-          </Link>
-          <ul className='right hide-on-med-and-down'>
-            <li>
-              <Link to='/Experience'>Experience</Link>
-            </li>
-            <li>
-              <Link to='/Teaching'>Teaching</Link>
-            </li>
-            <li>
-              <Link to='/Projects'>Projects</Link>
-            </li>
-            <li>
-              <Link to='/Resume'>Résumé</Link>
-            </li>
-            <li>
-              <Link to='/Contact'>Contact</Link>
-            </li>
-          </ul>
-          <ul className='sidenav' id='mobile-demo'>
-            <li>
-              <Link to='/Experience'>Experience</Link>
-            </li>
-            <li>
-              <Link to='/Teaching'>Teaching</Link>
-            </li>
-            <li>
-              <Link to='/Projects'>Projects</Link>
-            </li>
-            <li>
-              <Link to='/Resume'>Résumé</Link>
-            </li>
-            <li>
-              <Link to='/Contact'>Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <nav style={style}>
+      <div className='nav-wrapper'>
+        <Link to='/' className='brand-logo' style={{ padding: '0 20px' }}>
+          Adnan Reza
+        </Link>
+        <Link to='#' data-target='mobile-demo' className='sidenav-trigger'>
+          <i className='material-icons'>menu</i>
+        </Link>
+        <ul className='right hide-on-med-and-down'>
+          <li>
+            <Link to='/Experience'>Experience</Link>
+          </li>
+          <li>
+            <Link to='/Teaching'>Teaching</Link>
+          </li>
+          <li>
+            <Link to='/Projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/Resume'>Résumé</Link>
+          </li>
+          <li>
+            <Link to='/Contact'>Contact</Link>
+          </li>
+        </ul>
+        <ul className='sidenav' id='mobile-demo'>
+          <li>
+            <Link to='/Experience'>Experience</Link>
+          </li>
+          <li>
+            <Link to='/Teaching'>Teaching</Link>
+          </li>
+          <li>
+            <Link to='/Projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/Resume'>Résumé</Link>
+          </li>
+          <li>
+            <Link to='/Contact'>Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
