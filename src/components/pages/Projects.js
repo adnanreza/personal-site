dfbc5c55349a4abd7dc5947296ae3049f0d943c3
Adnan Reza <adnan.reza@outlook.com>
2019-10-06 10:35:20 -0700

    Changed text for projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,17 +2,18 @@ import React, { Fragment } from 'react';
 import ResumeBlock from '../layout/ResumeBlock';
 
 const Projects = () => {
-  const alphaGo = [
-    'While Go programs have been around since the 1970s, their performance has not come close to achieving professional level playing let alone defeating Go champions. AlphaGo’s journey towards achieving superhuman-level performance in Go came into the spotlight in 2016 when it defeated Go world champion Lee Sedol. While it is easy to be carried away by the enormous hype surrounding AlphaGo’s achievement, it is a worthwhile exercise to delve deeper into how AlphaGo works and in the process demystify some of the hype surrounding this truly extraordinary achievement. This is the primary motivation of this essay. By looking deeper into AlphaGo’s architecture, we can see that it is indeed a combination of techniques, some of which have been developed by researchers in the early days of computer Go, aided with state of the art hardware.'
+  
+  const devBook = [
+    'A social network for developers built using the MERN stack.'
   ];
-  const gt2 = [
-    'Network Bandwidth is an important shared resource like the CPU and Memory in datacenters. However, unlike other system resources, cloud providers currently do not provide hard guarantees on minimum bandwidth, and the allocations are often unfair. Nash bargaining has been extensively used in economics to achieve a middle-ground between fairness and efficiency in resource allocation problems. We describe GT2, a game-theoretic framework that models the data center bandwidth allocation problem as a bargaining game, and leverages the underlying theory to derive a Nash Bargaining Solution which achieves our objectives of minimum bandwidth guarantee and fairness.'
+  const contactOrganizer = [
+    'A contacts organizer built using MERN stack'
   ];
-  const ml = [
-    'This perspective paper presents the architecture of Convolutional Neural Networks (CNN). We discuss popular techniques used in neural nets in order to understand why these techniques help CNNs perform well at classification. Additionally, we discuss applications of CNNs and conclude by highlighting its strengths and weaknesses.'
+  const calTracker = [
+    'A calories tracker app built using vanilla JS using the module pattern'
   ];
-  const bgt = [
-    'In multi-agent settings, we often assume that agents will adopt Nash equilibrium strategies. However, studies in experimental economics demonstrate that Nash equilibrium is a poor indicator of human players’ initial behavior in normal-form games.[1] We study ways of improving the best performing behavioral models.'
+  const chickenRun = [
+    'ChickenRun is a web crawler written in PHP with a MySQL database.'
   ];
 
   const headingStyle = {
@@ -29,24 +30,28 @@ const Projects = () => {
             Projects
           </h4>
           <ResumeBlock
-            heading='Demystifying AlphaGo'
-            subheading='MSc Essay (Spring ‘18)'
-            body={alphaGo}
+            heading='devBook'
+            subheading={devBook}
+            body={devBook}
+            list={true}
           ></ResumeBlock>
           <ResumeBlock
-            heading='GT2: A game-theoretic framework for fair and efficient bandwidth allocation in datacenters'
-            subheading='CPSC 527: Computer Communication Protocols (Fall ‘17)'
-            body={gt2}
+            heading='contactOrganizer'
+            subheading={contactOrganizer}
+            body={contactOrganizer}
+            list={true}
           ></ResumeBlock>
           <ResumeBlock
-            heading='Understanding Convolutional Neural Networks'
-            subheading='CPSC 540: Machine Learning (Spring ‘17)'
-            body={ml}
+            heading='calTracker'
+            subheading={calTracker}
+            body={calTracker}
+            list={true}
           ></ResumeBlock>
           <ResumeBlock
-            heading='Improving Human Behavior Prediction in Simultaneous-Move Games'
-            subheading='CPSC 522: Artificial Intelligence II (Spring ‘16)'
-            body={bgt}
+            heading='chickenRun'
+            subheading={chickenRun}
+            body={chickenRun}
+            list={true}
           ></ResumeBlock>
         </div>
       </div>
