e186dd4d727fd0c4d6414ebca9ff8b271848bfb1
Adnan Reza <adnan.reza@outlook.com>
2022-10-20 15:56:39 -0700

    Attribution to Icons 8 added

--- src/components/layout/ThemeToggle.js
+++ src/components/layout/ThemeToggle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const ThemeToggle = ( {dark, setDark} ) => {
+    /**Icons taken from https://icons8.com/ */
     const moon = <img className="theme-icon" src={process.env.PUBLIC_URL+"/moon-white.png"} alt="moon-icon" width="30px" height="30px"/>;
     const sun = <img className="theme-icon" src={process.env.PUBLIC_URL+"/sun-white.png"} alt="sun-icon" width="30px" height="30px"/>;
     return (
