48443f615b9a2ca6aa786d2cf15ae9658d2ae718
Adnan Reza <adnan.reza@outlook.com>
2026-02-13 16:21:02 -0800

    Setup fresh vite build

--- src/tests/pages/Contact.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Contact from '../../components/pages/Contact';
-
-test('renders Contact page correctly', () => {
-  const wrapper = shallow(<Contact />);
-  expect(wrapper).toMatchSnapshot();
-});
